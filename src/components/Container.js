06828dc0b8b0446070f81ed586af17b98ffabbaa
jkobby <fuachieluke@gmail.com>
2021-12-05 13:54:11 +0000

    Added styled-components and created basic header component

---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Container = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Container
--- src/routes/MainLayout.js
+++ src/routes/MainLayout.js
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router"
+import Header from '../components/Header'
 
 const MainLayout = () => {
     return (
         <div className="App">
+            <Header>
             <h1>REST Countries API</h1>
+            </Header>
             <Outlet />
         </div>
     )
